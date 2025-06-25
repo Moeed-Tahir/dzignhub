ea17bae383df946ac6c71b440b36585d956fa717
Avik khan <75199596+itspsychocoder@users.noreply.github.com>
2025-06-25 18:20:09 +0500

    fix minor issues

--- src/components/auth/PassReset.jsx
+++ src/components/auth/PassReset.jsx
@@ -2,15 +2,19 @@
 import React, { useState } from "react";
 import OtpInput from "../ui/OtpInput";
 import { useRouter,useSearchParams } from "next/navigation";
+import { useUserStore } from "@/store/store";
 
 const PassReset = ({ isPassReset }) => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const email = searchParams.get('email'); // Get email from URL params
+    const {SetIsLogin, SetEmail, SetUserId} = useUserStore()
   
   const [otp, setOtp] = useState("");
   const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
     const [isLoading, setIsLoading] = useState(false);
+    const [isOtpSent, setIsOtpSent] = useState(false);
   
 
   const correctOtp = "123456";
@@ -36,9 +40,17 @@ const PassReset = ({ isPassReset }) => {
     const data = await response.json();
     console.log(data)
     if (data.type === "success") {
-      isPassReset
-      ? router.push(`/auth/new-password?token=${data.resetSessionToken}&email=${encodeURIComponent(email)}`)
-      : router.push("/profile/onboarding");
+      if(isPassReset) {
+        router.push(`/auth/new-password?token=${data.resetSessionToken}&email=${encodeURIComponent(email)}`)
+      }
+      else {
+        console.log(data)
+        localStorage.setItem("token", data.token);
+        SetIsLogin(true);
+        SetEmail(data.user.email);
+        SetUserId(data.user._id);
+        router.push("/profile/onboarding");
+      }
     }
     else {
       setError(data.message);
@@ -63,24 +75,34 @@ const PassReset = ({ isPassReset }) => {
 
 
   const handleResendOtp = async () => {
+    setIsOtpSent(true);
     let Email = decodeURIComponent(email);
     let apiEndpoint = isPassReset ? "forgot-password" : "resend-otp";
     console.log(apiEndpoint)
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/${apiEndpoint}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email: Email, isPasswordReset: isPassReset }),
-    });
-
-    const data = await response.json();
-    console.log(data)
-    if (data.type === "success") {
-      setError(""); // Clear any previous error
-      alert("OTP resent successfully!");
-    } else {
-      setError("Failed to resend OTP. Please try again.");
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/${apiEndpoint}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: Email, isPasswordReset: isPassReset }),
+      });
+  
+      const data = await response.json();
+      console.log(data)
+      if (data.type === "success") {
+        setError(""); // Clear any previous error
+        setSuccess("OTP resent Successfully")
+      } else {
+        setError("Failed to resend OTP. Please try again.");
+      }
+    }
+    catch(error) {
+      setError(error.message);
+      console.error(error)
+    }
+    finally {
+      setIsOtpSent(false);
     }
   }
 
@@ -101,10 +123,22 @@ const PassReset = ({ isPassReset }) => {
         {error && (
           <p className="text-red-500 text-sm mt-2 ml-3 text-start">{error}</p>
         )}
+        {
+          success && (
+            <p className="text-black text-sm mt-2 ml-3 text-start">{success}</p>
+          )
+        }
         <p className="text-[#44444A] mt-6 mb-3 text-sm text-center">
           Didn't get a code?
           <button onClick={handleResendOtp} className="ml-1 underline cursor-pointer" type="button">
-            Click to resend
+        
+            {
+              isOtpSent? (
+                <div className="animate-spin rounded-full h-5 w-5 border-b-2 border-black"></div>
+              ) : (
+                "Click to resend"
+              )}
+            
           </button>
         </p>
       
