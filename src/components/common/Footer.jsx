6f2a58a53bc73a07549579278bafdd35e45126db
Faiq Ahmad <faiqueahmad476@gmail.com>
2025-06-25 23:33:01 +0500

    Done with footer's responsiveness

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -80,26 +80,26 @@ const syne = Syne({
 function Footer() {
   return (
     <div className="w-full bg-[#1B1F3B] text-[#EDEDED]">
-      <div className="w-[1440px] flex flex-col gap-[64px] py-[64px] px-[80px]">
-        <div className="w-[1280px] flex justify-between">
-          <div className="w-[320px] flex flex-col gap-[32px]">
+      <div className="max-w-[1440px] mx-auto gap-[42px] border-2 md:py-[64px] md:px-[80px] py-[32px] px-[24px] md:gap-[64px]   flex flex-col ">
+        <div className="max-w-[1280px] w-full mx-auto  gap-[42px] md:gap-[64px]  flex md:flex-row flex-wrap flex-col justify-between">
+          <div className="max-w-[320px] w-full border-black  flex flex-col  gap-[32px]">
             <img
               src="/common/footer/logo-with-name.svg"
-              className="w-[185.07px] h-[35.26px]"
+              className="max-w-[185.07px]  max-h-[35.26px]"
             />
-            <div className="flex gap-[16px] h-[40px]">
+            <div className="flex justify-between    max-h-[40px]">
               {socialLinks.map(({ alt, icon, href }, index) => (
                 <Link key={index} href={href}>
-                  <img src={icon} alt={alt} className="h-[40px] w-auto" />
+                  <img src={icon} alt={alt} className=" w-auto" />
                 </Link>
               ))}
             </div>
           </div>
-          <div className="flex gap-[120px]">
+          <div className="flex  flex-wrap gap-[120px]">
             {footerLinks.map((section, i) => (
               <div
                 key={i}
-                className={`flex flex-col gap-[13px] ${roboto.classname}  text-[#FFFFFF]`}
+                className={`flex flex-col gap-[13px] ${roboto.className}  text-[#FFFFFF]`}
               >
                 <h2 className="text-[20px] font-semibold">{section.title}</h2>
                 {section.links.map((link, j) => (
@@ -111,9 +111,9 @@ function Footer() {
             ))}
           </div>
         </div>
-        <div className="w-[1280px] flex flex-col gap-[16px] border-t border-[#E0E0E0]">
-          <div className="w-full flex flex-col gap-[24px] items-center">
-            <div className="flex gap-[24px] h-[40px] mx-auto items-center font-roboto text-[11.63px]">
+        <div className="max-w-[1280px] w-full mx-auto  flex flex-col gap-[16px] border-t border-[#E0E0E0]">
+          <div className=" flex flex-col gap-[24px] py-[16px]  items-center">
+            <div className="flex gap-[24px] flex-wrap   mx-auto items-center text-[11.63px]">
               {policyLinks.map(({ label, href }, index) => (
                 <Link key={index} href={href}>
                   {label}
@@ -125,16 +125,12 @@ function Footer() {
           </div>
         </div>
         <div
-          className={`w-[1280px] h-[26px] flex justify-between text-[16px] text-[#FFFFFF] ${syne.className} `}
+          className={`max-w-[1280px] w-full mx-auto gap-[24px]  flex flex-col md:flex-row justify-between  flex-wrap text-[16px] text-[#FFFFFF] ${syne.className} `}
           style={{}}
         >
-          <div className="h-full flex gap-[24px] ">
+          <div className="h-full flex flex-col md:flex-row gap-[24px] ">
             {navItems.map(({ label, href }, idx) => (
-              <Link
-                key={idx}
-                href={href}
-                className="hover:underline"
-              >
+              <Link key={idx} href={href} className="hover:underline">
                 {label}
               </Link>
             ))}
